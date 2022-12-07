ca113bead052ce232d97417050b2e3e7b951796c
Mateusz <m.maj6743@gmail.com>
2022-12-08 00:48:17 +0100

    [feature] Added footer

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,4 +1,5 @@
 import { Route, Routes } from "react-router-dom";
+import Footer from "../components/Footer";
 import BrowsePage from "../pages/BrowsePage/BrowsePage";
 import LandingPage from "../pages/LandingPage";
 import LoginPage from "../pages/LoginPage";
@@ -7,13 +8,16 @@ import RegisterPage from "../pages/RegisterPage";
 
 const App = () => {
   return (
-    <Routes>
-      <Route element={<LandingPage />} path="/" />
-      <Route element={<LoginPage />} path="/login" />
-      <Route element={<RegisterPage />} path="/register" />
-      <Route element={<BrowsePage />} path="/browse" />
-      <Route element={<NftsPage />} path="/browse/:id" />
-    </Routes>
+    <div className="pb-44">
+      <Routes>
+        <Route element={<LandingPage />} path="/" />
+        <Route element={<LoginPage />} path="/login" />
+        <Route element={<RegisterPage />} path="/register" />
+        <Route element={<BrowsePage />} path="/browse" />
+        <Route element={<NftsPage />} path="/browse/:id" />
+      </Routes>
+      <Footer />
+    </div>
   );
 };
 
---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,42 @@
+import { ReactComponent as MarketIcon } from "../../assets/icons/marketIcon.svg";
+
+const Footer = () => {
+  return (
+    <footer className="px-20 flex gap-5 absolute bottom-14 w-full justify-center">
+      <div className="flex flex-col gap-6 w-80">
+        <span className="flex font-bold justify-center gap-2 items-center font-mono">
+          <MarketIcon />
+          <h3>NFT Marketplace</h3>
+        </span>
+        <span className="text-gray text-center">
+          NFT Marketplace for schools created for Warsaw University of
+          Technology
+        </span>
+      </div>
+      <div className="flex flex-col items-center gap-3 w-80">
+        <span className="font-bold font-mono text-xl">Explore</span>
+        <span className="text-gray">Marketplace</span>
+        <span className="text-gray">Schools</span>
+        <span className="text-gray">Login</span>
+      </div>
+      <div className="flex flex-col gap-3 w-80">
+        <span className="font-bold font-mono text-xl">Join newsletter</span>
+        <span className="text-gray">
+          Get exclusive promotions & updates about NFTs straight to your inbox
+        </span>
+        <span className="flex gap-2">
+          <input
+            type="text"
+            placeholder="Enter your email"
+            className="input w-72"
+          />
+          <button className="btn bg-secondary hover:bg-secondaryHoverFocus focus:bg-secondaryHoverFocus w-fit">
+            Subscribe
+          </button>
+        </span>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
