63b29c9392ea1674cbb9859a438d8b6c3fbc8541
Piotr Brzeziński <thewildtree@outlook.com>
2023-01-17 15:37:23 +0100

    Logout local user upon a 401 response

--- src/app/AppProviders.tsx
+++ src/app/AppProviders.tsx
@@ -1,14 +1,28 @@
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { MutationCache, QueryCache, QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import axios from "axios";
 import { BrowserRouter } from "react-router-dom";
+import { useAuthStore } from "../stores/AuthStore";
 
 type Props = {
   children: JSX.Element;
 };
 
-axios.defaults.withCredentials = true
+axios.defaults.withCredentials = true;
 
-const queryClient = new QueryClient();
+const axiosErrorCallback = (error: any, query: any) => {
+  if (error.response.status === 401) {
+    useAuthStore.getState().logoutUser();
+  }
+};
+
+const queryClient = new QueryClient({
+  queryCache: new QueryCache({
+    onError: axiosErrorCallback,
+  }),
+  mutationCache: new MutationCache({
+    onError: axiosErrorCallback,
+  }),
+});
 
 const AppProviders = ({ children }: Props) => (
   <QueryClientProvider client={queryClient}>
