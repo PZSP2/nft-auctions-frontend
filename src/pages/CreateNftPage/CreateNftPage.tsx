ea767622df58de1fa5122f8effc0da91b5810adc
Mateusz <m.maj6743@gmail.com>
2023-01-19 00:54:49 +0100

    [feature] Added tags for creating NFTs

--- src/pages/CreateNftPage/CreateNftPage.tsx
+++ src/pages/CreateNftPage/CreateNftPage.tsx
@@ -1,20 +1,49 @@
-import { useMutation } from "@tanstack/react-query";
+import { useMutation, useQuery } from "@tanstack/react-query";
 import axios from "axios";
-import { useState } from "react";
+import { useMemo, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { API_KEYS } from "../../api/API_KEYS";
+
+type Tag = {
+  tagId: number;
+  name: string;
+  selected: boolean;
+};
 
 const CreateNftPage = () => {
   const [name, setName] = useState("");
   const [description, setDescription] = useState("");
+  const [availableTags, setAvailableTags] = useState<Tag[]>();
   const [file, setFile] = useState<File | null>(null);
   const navigate = useNavigate();
-  const canCreate = name && description && file;
+  const selectedTags = useMemo(
+    () =>
+      availableTags
+        ?.filter(({ selected }) => selected)
+        .map(({ tagId }) => tagId),
+    [availableTags]
+  );
+  const canCreateNft =
+    name && description && file && selectedTags && selectedTags?.length > 0;
 
   const { mutateAsync } = useMutation(
     [API_KEYS.CREATE_NFT],
     (formData: FormData) => axios.post("/api/nft", formData),
     { onSuccess: () => navigate("/ownedNfts") }
+  );
+  const { data: tagsResponse } = useQuery(
+    [API_KEYS.GET_TAGS],
+    () => axios.get<Omit<Tag, "selected">[]>("/api/tag").then((res) => res),
+    {
+      onSuccess: (res) =>
+        setAvailableTags(
+          res.data.map((tag) => ({
+            selected: false,
+            tagId: tag.tagId,
+            name: tag.name,
+          }))
+        ),
+    }
   );
 
   const handleNameChange = (e: React.ChangeEvent<HTMLInputElement>) =>
@@ -29,15 +58,16 @@ const CreateNftPage = () => {
   };
 
   const handleSubmit = async () => {
-    if (!name || !description || !file) return;
+    if (!name || !description || !file || !selectedTags) return;
     const formData = new FormData();
+    selectedTags.forEach((tag) => formData.append("tags", tag.toString()));
     formData.append("file", file);
     formData.append("name", name);
     formData.append("description", description);
     mutateAsync(formData);
   };
 
-  return (
+  return tagsResponse ? (
     <main className="py-32 px-20 flex items-start flex-col justify-center">
       <div className="flex justify-around w-full">
         <span>
@@ -48,7 +78,7 @@ const CreateNftPage = () => {
         </span>
         <button
           className="btn btn-primary"
-          disabled={!canCreate}
+          disabled={!canCreateNft}
           onClick={handleSubmit}
         >
           Create an NFT
@@ -75,9 +105,46 @@ const CreateNftPage = () => {
             className="file-input w-full max-w-xs"
             onChange={handleFileChange}
           />
+          <div className="dropdown">
+            <label tabIndex={0} className="btn m-1">
+              Select tags
+            </label>
+            <ul
+              tabIndex={0}
+              className="dropdown-content menu p-2 shadow bg-base-100 rounded-box w-52 flex-nowrap max-h-64 overflow-y-scroll"
+            >
+              {availableTags?.map((tag) => (
+                <li key={tag.tagId}>
+                  <label className="label cursor-pointer">
+                    <span className="label-text">{tag.name}</span>
+                    <input
+                      type="checkbox"
+                      checked={tag.selected}
+                      onChange={() =>
+                        setAvailableTags(
+                          availableTags.map((item) =>
+                            item.tagId === tag.tagId
+                              ? {
+                                  name: item.name,
+                                  tagId: item.tagId,
+                                  selected: !item.selected,
+                                }
+                              : item
+                          )
+                        )
+                      }
+                      className="checkbox checkbox-primary"
+                    />
+                  </label>
+                </li>
+              ))}
+            </ul>
+          </div>
         </div>
       </section>
     </main>
+  ) : (
+    <progress className="progress w-56" />
   );
 };
 
