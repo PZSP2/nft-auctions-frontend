527e78a5218baa6a5fd73a606763815e24e738a6
Piotr Brzeziński <thewildtree@outlook.com>
2023-01-19 02:51:35 +0100

    Fix account updates querying without user logged in

--- src/hooks/useAccountUpdates.ts
+++ src/hooks/useAccountUpdates.ts
@@ -1,10 +1,14 @@
 import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
 import { API_KEYS } from "../api/API_KEYS";
+import { useAuthStore } from "../stores/AuthStore";
 
-export const useAccountUpdates = () =>
-  useQuery([API_KEYS.GET_ACCOUNT_UPDATES], () => {
+export const useAccountUpdates = () => {
+  const { isUserLoggedIn } = useAuthStore();
+
+  return useQuery([API_KEYS.GET_ACCOUNT_UPDATES], () => {
     return axios
       .get("/api/account/me/updates")
       .then((response) => response);
-  });
+  }, { enabled: isUserLoggedIn() });
+};
