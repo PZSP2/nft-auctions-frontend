e5196855693afcd61be081f03bc8eebf9e6a359a
Mateusz <m.maj6743@gmail.com>
2023-01-19 02:36:21 +0100

    [bugfix] Updated wallet fetching

--- src/hooks/useWallet.ts
+++ src/hooks/useWallet.ts
@@ -1,8 +1,13 @@
 import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
 import { API_KEYS } from "../api/API_KEYS";
+import { useAuthStore } from "../stores/AuthStore";
 
-export const useWallet = () =>
-  useQuery([API_KEYS.GET_WALLET], () =>
-    axios.get("/api/account/wallet").then((response) => response)
+export const useWallet = () => {
+  const { isUserLoggedIn } = useAuthStore();
+  return useQuery(
+    [API_KEYS.GET_WALLET],
+    () => axios.get("/api/account/wallet").then((response) => response),
+    { enabled: isUserLoggedIn() }
   );
+};
